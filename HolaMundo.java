c833c04c28e3738bb668d08bed0a5f2ec77326c6
Alex LC <aleal@iealixar.org>
2021-09-27 14:07:33 +0200

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+* Muestra por pantalla la frase ¡Hola mundo!
+*
+* @author alejandrolc
+**/
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola mundo!");
